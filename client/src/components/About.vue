9c51f46e78e6731887d0fd6ae384334a0c263697
Hengle Jiang <jianghengle@gmail.com>
2018-12-05 10:28:59 -0600

    make introduction md

--- client/src/components/About.vue
+++ client/src/components/About.vue
@@ -15,7 +15,7 @@
           <img src="static/logos/usignite.png" class="long-logo">
         </div>
         <div class="content">
-          <p>The NEXTT pilot testbed is a novel, Cloud-Radio Access Network (C-RAN)-based experimental wireless network. The testbed contains three major components: Software Defined Distribution Units (SD-DU) (a.k.a Cognitive Remote Radio Heads), fiber Ethernet-based fronthaul network, and cloud-based baseband processing and data storage. Currently, there are 5 SD-DUs deployed at 4 buildings on UNL campus and 1 street light in Lincoln city. Via Fiber Ethernet-based fronthaul network, these SD-DUs are connected to Holland Computing Center (HCC), where baseband processing and data storage are taken place. The core of SD-DU is a high-end 4x4 MIMO Software Defined Radio (SDR) Transceiver, Ettus N310, which can be tuned to any sub-6GHz frequency. The NEXTT pilot testbed allows researchers to access its radio sites and computing resources remotely.</p>
+          <vue-markdown :source="introduction"></vue-markdown>
         </div>
       </div>
       <div class="image-container has-text-centered">
@@ -27,13 +27,24 @@
 
 <script>
 import Vue from 'vue'
+import VueMarkdown from 'vue-markdown'
 
 export default {
   name: 'about',
+  components: {
+    VueMarkdown
+  },
   data () {
     return {
-      data: '',
+      introduction: '',
     }
+  },
+  mounted () {
+    this.$http.get(xSTATICx + 'docs/introduction.md').then(response => {
+      this.introduction = response.data
+    }, response => {
+      console.log('failed to get intro md')
+    })
   }
 }
 </script>
